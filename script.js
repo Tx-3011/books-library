3b4c40c0ca1a15e1bf6848312d9d894ac133416f
Tejdeep Nookala <iamtejdeepn@gmail.com>
2024-07-31 20:14:50 +0530

    added a basic object structure

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const allBooks = document.querySelector("#allBooks");
 
 let myLibrary = [];
 
-function Book(title){   //,author,pages,read
+function Book(title,author,pages,read){   //,author,pages,read
 
     this.title = title;
-    // this.author = author;
-    // this.pages = pages;
-    // this.read = read;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
     
 }
 
@@ -18,11 +18,11 @@ function Book(title){   //,author,pages,read
 
 function addBookToLibrary(){
     let name1 = prompt("Enter the book name: ")
-    // let author1 = prompt("Enter the author name: ")
-    // let pages1 = prompt("Enter the number of pages: ")
-    // let read1 = prompt("Did you read the book?")
+    let author1 = prompt("Enter the author name: ")
+    let pages1 = prompt("Enter the number of pages: ")
+    let read1 = prompt("Did you read the book?")
 
-    myLibrary.push(new Book(name1));  //,author1,pages1,read1
+    myLibrary.push(new Book(name1,author1,pages1,read1));  
 
     renderBooks()
 
@@ -33,10 +33,44 @@ function addBookToLibrary(){
 function renderBooks(){
     allBooks.textContent = ``
     for(let i=0;i<myLibrary.length;i++){
-        let renderedBook = document.createElement("p")
-        renderedBook.textContent = `Name: ${myLibrary[i].title}`
 
-        allBooks.appendChild(renderedBook);
+        let renderedDiv = document.createElement("div")
+        renderedDiv.className = "bookCard"
+
+
+
+        let renderedTitle = document.createElement("p")
+        renderedTitle.className = "title"
+        renderedTitle.textContent= `${myLibrary[i].title}`
+
+        let renderedAuthor = document.createElement("p")
+        renderedAuthor.className = "author"
+        renderedAuthor.textContent= `${myLibrary[i].author}`
+
+        let renderedPages = document.createElement("p")
+        renderedPages.className = "pages"
+        renderedPages.textContent = `${myLibrary[i].pages}`
+
+        let renderedRead = document.createElement("p")
+        renderedRead.className = "readStatus"
+        renderedRead.textContent = `${myLibrary[i].read}`
+
+
+
+        // renderedBook.textContent = `Name: ${myLibrary[i].title}
+        //                             Author:${myLibrary[i].author}
+        //                             Pages:${myLibrary[i].pages}
+        //                             ReadStatus:${myLibrary[i].read}`
+
+        renderedDiv.appendChild(renderedTitle);
+        renderedDiv.appendChild(renderedAuthor);
+        renderedDiv.appendChild(renderedPages);
+        renderedDiv.appendChild(renderedRead);
+
+        allBooks.appendChild(renderedDiv);
+
+
+
     }
 }
 
